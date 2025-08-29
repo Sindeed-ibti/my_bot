dc70919676303c9ee9515638983ba1c315d5d66a
Sindeed-ibti <sindeed.alam.ibti@g.bracu.ac.bd>
2025-08-29 20:38:12 +0600

    Updated

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -18,6 +18,13 @@ def generate_launch_description():
     # !!! MAKE SURE YOU SET THE PACKAGE NAME CORRECTLY !!!
 
     package_name='my_bot' #<--- CHANGE ME
+    
+    world_file_name = 'obstacles.world'
+    world_path = os.path.join(
+        get_package_share_directory(package_name),
+        'worlds',
+        world_file_name
+    )
 
     rsp = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
@@ -28,7 +35,7 @@ def generate_launch_description():
     # Include the Gazebo launch file, provided by the gazebo_ros package
     gazebo = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
+                    get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),  launch_arguments={'world': world_path}.items()
              )
 
     # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
